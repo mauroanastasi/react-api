1ddc4a3b1cab4c4b718477de02e709af7d448aee
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-13 18:24:48 +0200

    [UPD] MIL-1. con un pulsante a cui assego una funzione al onClick, vado a richiamare questa funzione che stampa a console (ajax call) l'elenco delle attirci contenuto in response.data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import axios from 'axios'
 import { useState } from 'react'
 
+
 function App() {
+  const [actresses, setActresses] = useState([]);
+
+  // creo la funzione che conterrà la chiamata ajax
+  const fetchActresses = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((response) => console.log(response.data));
+  }
 
   return (
     <>
-
+      <button className="btn btn-primary" onClick={fetchActresses}>stampa a console</button>
     </>
   )
 }
